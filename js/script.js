0c1cc9859aa6ba3f9ad4d9c4b8909a226c1ab007
ismail-karmout <issmailkarmout@gmail.com>
2024-09-20 19:45:50 +0100

    Updating Portfolio Master Degree

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
     let footer = document.querySelector('footer');
-    footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+    // footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
 
